83b52fdcc0c97900e09af1a07ad66bcdce75d5f7
mbarker-byte <m.barker94@hotmail.co.uk>
2026-08-18 20:07:38 +0100

    Refactor fish finder with CSV support and new functions

--- FishApp.py
+++ FishApp.py
@@ -1,22 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 17 20:27:00 2026
-
-@author: MBark
-"""
-
 import os
 import time
+import csv
 
 clear = lambda: os.system('cls')
 
-testFish = {"season" : "Winter", "weather" : "Rain", "time" : "Morning", "area" : "Beach", "name" : "Test Fish 1" } 
+testFish = {"season" : "Winter", "weather" : "Rain", "time" : "Morning", "name" : "Test Fish 1" } 
+fishList = []
 
-def findFish():
-    print ("Welcome to the fish finder!")
-    time.sleep(2)
-    print("Please enter information when prompted.")
-    time.sleep(2)
+with open('fish.csv') as csvfile:
+    fishReader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for fish in fishReader:
+        fishList.append(fish)
+        
+def fishSeason():
     print("What season is it?")
     time.sleep(1)
     print("Spring: A")
@@ -26,9 +22,15 @@ def findFish():
     print("Autumn: C")
     time.sleep(1)
     print("Winter: D")
-    fishSeason = input("Please choose a season:")
+    fishSeason = input("Please choose a season:").lower()
+    if fishSeason not in ('a', 'b', 'c', 'd'):
+        print("Invalid option, please try again.")
+        fishSeason()
     clear()
     time.sleep(2)
+        return fishSeason
+    
+def fishTime():
     print("What time is it?")
     time.sleep(1)
     print("Morning: A")
@@ -37,7 +39,36 @@ def findFish():
     time.sleep(1)
     print("Night: C")
     time.sleep(1)
-    fishTime = input("Please choose a time:")
+    fishTime = input("Please choose a time:").lower()
     clear()
     time.sleep(2)
+    return fishTime
     
+def fishWeather():
+    print("Please choose the current weather:")
+    time.sleep(1)
+    print("Rain/Snow: A")
+    time.sleep(1)
+    print("Clear: B")
+    time.sleep(1)
+    print("Stormy: C")
+    time.sleep(1)
+    fishWeather = input("Please choose the weather.").lower()
+    clear()
+    time.sleep(2)
+    return fishWeather
+
+            
+
+def findFish():
+    print ("Welcome to the fish finder!")
+    time.sleep(2)
+    print("Please enter information when prompted.")
+    time.sleep(2)
+    fishSeason()
+    
+    fishTime()
+    fishWeather()
+    
+    for fish in fishList:
+        if
