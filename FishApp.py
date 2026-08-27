7df74c165e05b4d81fe788d4ba121603d3987a47
mbarker-byte <m.barker94@hotmail.co.uk>
2026-08-27 20:49:52 +0100

    Enhance fish finder with legendary fish feature
    Refactor fish finder application to include legendary fish option and improve user prompts.

--- FishApp.py
+++ FishApp.py
@@ -5,6 +5,7 @@ import csv
 clear = lambda: os.system('cls')
 
 #TODO add logic for multiple values per key and any values: change identifying if statements to contains rather than ==
+
 testFish = [{"season" : "Summer", "time" : "Afternoon", "weather" : "Sun", "name" : "Pufferfish", "area" : "Ocean/Island" },
             {"season" : "Spring/Fall", "time" : "Anytime", "weather" : "Any", "name" : "Anchovy", "area" : "Ocean" },
             {"season" : "Summer/Winter", "time" : "Morning/Afternoon", "weather" : "Any", "name" : "Tuna", "area" : "Ocean/Island" },
@@ -16,6 +17,8 @@ testFish = [{"season" : "Summer", "time" : "Afternoon", "weather" : "Sun", "name
             {"season" : "", "time" : "", "weather": "", "name" : "", "area" : ""},
             {"season" : "", "time" : "", "weather": "", "name" : "", "area" : ""},
             ]
+
+legendaryFish=[]
 
 def getFish():
     fishList = []
@@ -58,9 +61,12 @@ def fishSeason():
         time.sleep(2)
         clear()
         fishSeason()
-    clear()
-    time.sleep(2)
-    return fishSeason
+    else:
+        print("Thanks for choosing a season!")
+        time.sleep(2)
+        clear()
+        time.sleep(2)
+        return fishSeason
     
 def fishTime():
     print("What time is it?")
@@ -77,9 +83,12 @@ def fishTime():
         time.sleep(2)
         clear()
         fishSeason()
-    clear()
-    time.sleep(2)
-    return fishTime
+    else:
+        print("Thanks for choosing a time!")
+        time.sleep(2)
+        clear()
+        time.sleep(2)
+        return fishTime
     
 def fishWeather():
     print("What is the weather like?")
@@ -96,15 +105,43 @@ def fishWeather():
         time.sleep(2)
         clear()
         fishSeason()
-    clear()
-    time.sleep(2)
-    return fishWeather
+    else:
+        print("Thanks for choosing the weather!")
+        time.sleep(2)
+        clear()
+        time.sleep(2)
+        return fishWeather
 
             
 
-def findFish():
+def fishMenu():
     print ("Welcome to the fish finder!")
     time.sleep(2)
+    print ("Do you want to find legendary or regular fish?")
+    time.sleep(1)
+    print("Legendary: A")
+    time.sleep(1)
+    print("Regular: B")
+    time.sleep(1)
+    choice = input("Please Choose your fish type.").lower()
+    if choice not in ('a', 'b'):
+        print ("invalid option, please try again.")
+        time.sleep(2)
+        clear()
+        fishMenu()
+    else:
+        if choice == 'a':
+            print ("Loading legendary fish...")
+            time.sleep(2)
+            clear()
+            legendaryFish()
+        if choice =='b':
+            print ("Loading regular fish...")
+            time.sleep(2)
+            clear()
+            regularFish()
+        
+def regularFish():
     print("Please enter information when prompted.")
     time.sleep(2)
     s = fishSeason()    
@@ -131,6 +168,9 @@ def findFish():
     print ("would you like to go again?")
     answer = input("Y/N?").lower()
     if answer == "y":
-        findFish()
+        fishMenu()
     else:
         return
+    
+def legendaryFish():
+    print("Please enter information when prompted")
