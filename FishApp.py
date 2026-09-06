12953004010a696d9423e84835e4ec8bddcc7c95
mbarker-byte <m.barker94@hotmail.co.uk>
2026-09-06 22:57:17 +0100

    Enhance fish filtering logic and update TODOs
    Added logic to filter fish based on season, time, and weather. Updated comments and added a TODO for caught fish logic.

--- FishApp.py
+++ FishApp.py
@@ -8,6 +8,7 @@ clear = lambda: os.system('cls')
 #TODO use lists instead of strings for values
 #TODO Change Strings in the fish dictionary to alphanumeric options, have a seperate option dictionary to bind values to strings
 #TODO Use a CSV File for fish rather than an in file dictionary.
+#TODO Add caught value to CSV to check so that only uncaught fish are returned
 
 testFish = [{"season" : "Summer", "time" : "Afternoon", "weather" : "Sun", "name" : "Pufferfish", "area" : "Ocean/Island" },
             {"season" : "Spring/Fall", "time" : "Anytime", "weather" : "Any", "name" : "Anchovy", "area" : "Ocean" },
